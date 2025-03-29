da767fc735beb74bb15d95561c4ddc5630d30b52
paulustimothy <paulustimothy011@gmail.com>
2025-03-29 16:10:02 +0700

    3. fetch posts for the home page

--- frontend/src/components/common/Posts.jsx
+++ frontend/src/components/common/Posts.jsx
@@ -1,24 +1,59 @@
+import { useEffect } from "react";
 import Post from "./Post";
 import PostSkeleton from "../skeletons/PostSkeleton";
-import { POSTS } from "../../utils/db/dummy";
+import { useQuery } from "@tanstack/react-query";
 
-const Posts = () => {
-	const isLoading = false;
+const Posts = ({feedType}) => {
+
+	const getPostEndpoint = () => {
+		switch(feedType) {
+			case "following":
+				return "/api/posts/following";
+			case "for-you":
+				return "/api/posts/all";
+			default:
+				return "/api/posts/all";
+		}
+	}
+
+	const POST_ENDPOINT = getPostEndpoint();
+
+	const {data:posts, isLoading, refetch, isRefetching} = useQuery({
+		queryKey: ["posts"],
+		queryFn: async () => {
+			try {
+				const res = await fetch(POST_ENDPOINT);
+				const data = await res.json();
+
+				if(!res.ok) {
+					throw new Error(data.error || "Failed to fetch posts");
+				}
+
+				return data;
+
+			} catch (error) {
+				throw new Error(error);
+			}
+		}
+	});
+	useEffect(() => {
+		refetch();
+	}, [feedType, refetch]);
 
 	return (
     // TODO learn all the classes
 		<>
-			{isLoading && (
+			{(isLoading || isRefetching) && (
 				<div className='flex flex-col justify-center'>
 					<PostSkeleton />
 					<PostSkeleton />
 					<PostSkeleton />
 				</div>
 			)}
-			{!isLoading && POSTS?.length === 0 && <p className='text-center my-4'>No posts in this tab. Switch 👻</p>}
-			{!isLoading && POSTS && (
+			{!isLoading && !isRefetching && posts?.length === 0 && <p className='text-center my-4'>No posts in this tab. Switch 👻</p>}
+			{!isLoading && !isRefetching && posts && (
 				<div>
-					{POSTS.map((post) => (
+					{posts.map((post) => (
 						<Post key={post._id} post={post} />
 					))}
 				</div>
--- frontend/src/components/skeletons/PostSkeleton.jsx
+++ frontend/src/components/skeletons/PostSkeleton.jsx
@@ -1,14 +1,28 @@
 const PostSkeleton = () => {
 	return (
-		<div className='flex flex-col gap-4 w-full p-4'>
-			<div className='flex gap-4 items-center'>
-				<div className='skeleton w-10 h-10 rounded-full shrink-0'></div>
-				<div className='flex flex-col gap-2'>
-					<div className='skeleton h-2 w-12 rounded-full'></div>
-					<div className='skeleton h-2 w-24 rounded-full'></div>
+		<div className='flex gap-2 items-start p-4 border-b border-gray-700'>
+			<div className='w-8'>
+				<div className='skeleton w-8 h-8 rounded-full'></div>
+			</div>
+			<div className='flex flex-col flex-1'>
+				<div className='flex gap-2 items-center'>
+					<div className='skeleton h-4 w-20'></div>
+					<div className='skeleton h-3 w-32'></div>
+				</div>
+				<div className='flex flex-col gap-3 mt-2'>
+					<div className='skeleton h-16 w-full'></div>
+				</div>
+				<div className='flex justify-between mt-3'>
+					<div className='flex gap-4 items-center w-2/3 justify-between'>
+						<div className='skeleton h-4 w-8'></div>
+						<div className='skeleton h-4 w-8'></div>
+						<div className='skeleton h-4 w-8'></div>
+					</div>
+					<div className='flex w-1/3 justify-end'>
+						<div className='skeleton h-4 w-4'></div>
+					</div>
 				</div>
 			</div>
-			<div className='skeleton h-40 w-full'></div>
 		</div>
 	);
 };
